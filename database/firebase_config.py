bba7f4929988d733a848a14cada2579c94e32d8f
Syed Asad Abbas Rizvi <111962788+AsadAbbas026@users.noreply.github.com>
2025-12-24 18:33:06 +0500

    Update Firebase init to use dynamic credentials path
    Refactor Firebase initialization to use environment variable for credentials path.

--- database/firebase_config.py
+++ database/firebase_config.py
@@ -1,17 +1,24 @@
 import firebase_admin
-from firebase_admin import credentials, db, storage
+from firebase_admin import credentials, db
 import os
 from dotenv import load_dotenv
-import datetime
 
 load_dotenv()
+
 def init_firebase():
-    if not firebase_admin._apps:
-        cred = credentials.Certificate(os.path.join("config", "smart-health-hub-93ab1-firebase-adminsdk-fbsvc-7bec0b6390.json"))
-        firebase_admin.initialize_app(cred, {
-            "databaseURL": os.getenv("DATABASE_URL")
-        })
+    if firebase_admin._apps:
+        return
+
+    cred_path = os.getenv(
+        "FIREBASE_CREDENTIALS_PATH",
+        "/etc/secrets/firebase-service-account.json"
+    )
+
+    cred = credentials.Certificate(cred_path)
+
+    firebase_admin.initialize_app(cred, {
+        "databaseURL": os.getenv("DATABASE_URL")
+    })
 
 def get_chat_ref(patient_id, doctor_id):
     return db.reference(f"chats/{patient_id}/{doctor_id}/messages")
-
